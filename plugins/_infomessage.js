01b349437f110455f733b95698b345c530b87aee
jerem146 <vasquezever146@gmail.com>
2025-12-22 04:34:30 -0500

    Actualizar _infomessage.js

--- plugins/_infomessage.js
+++ plugins/_infomessage.js
@@ -15,7 +15,31 @@ const chat = global.db.data.chats[m.chat]
 const users = m.messageStubParameters[0]
 const usuario = await resolveLidToRealJid(m?.sender, conn, m?.chat)
 const groupAdmins = participants.filter(p => p.admin)
-const rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: "𐔌 . ⋮ ᗩ ᐯ I Տ O .ᐟ ֹ ₊ ꒱", body: textbot, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
+let nombreGrupo = (await conn.groupMetadata(m.chat)).subject
+
+const rcanal = { 
+contextInfo: { 
+isForwarded: true, 
+forwardedNewsletterMessageInfo: { 
+newsletterJid: channelRD.id, 
+serverMessageId: '', 
+newsletterName: channelRD.name 
+}, 
+
+externalAdReply: { 
+title: `𐔌 . ⋮ ᗩ ᐯ I Տ O .ᐟ ֹ ₊ ꒱ — ${nombreGrupo}`, 
+body: textbot, 
+mediaUrl: null, 
+description: null, 
+previewType: "PHOTO", 
+thumbnail: await (await fetch(icono)).buffer(), 
+sourceUrl: redes, 
+mediaType: 1, 
+renderLargerThumbnail: false 
+}, 
+
+mentionedJid: null 
+}}
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/99j39u.png'
 const nombre = `> ❀ @${usuario.split('@')[0]} Ha cambiado el nombre del grupo.\n> ✦ Ahora el grupo se llama:\n> *${m.messageStubParameters[0]}*.`
 const foto = `> ❀ Se ha cambiado la imagen del grupo.\n> ✦ Acción hecha por:\n> » @${usuario.split('@')[0]}`
