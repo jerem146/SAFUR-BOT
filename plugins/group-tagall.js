b0ff2bf073f674888e959bd8ce00d0ba312d7a98
jerem146 <vasquezever146@gmail.com>
2025-12-29 22:07:31 -0500

    Actualizar group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,12 +1,32 @@
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
 const pesan = args.join` `
 const oi = `*» INFO :* ${pesan}`
-let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname}≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`
+
+let teks = `*!  MENCION GENERAL  !*\n`
+teks += `*PARA ${participants.length} MIEMBROS* 🗣️\n\n`
+teks += `${oi}\n\n`
+teks += `╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname}≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`
+
 for (const mem of participants) {
 teks += `┊ꕥ @${mem.id.split('@')[0]}\n`
 }
+
 teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄⸼`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) })
+
+// 📸 Obtener foto del grupo
+let pp = null
+try {
+pp = await conn.profilePictureUrl(m.chat, 'image')
+} catch {
+pp = 'https://i.imgur.com/8fK4h6q.png' // imagen por defecto si no hay foto
+}
+
+// 📤 Enviar con imagen del grupo
+await conn.sendMessage(m.chat, {
+image: { url: pp },
+caption: teks,
+mentions: participants.map(a => a.id)
+}, { quoted: m })
 }
 
 handler.help = ['todos']
